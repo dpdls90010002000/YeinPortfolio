e085c41c8ca62ec7c1fee19b13dc19b05d0862cb
dpdls90010002000 <dpdls9001000@gmail.com>
2023-10-02 23:11:49 -0400

    firstcommit

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 var app = express();
 // app.listen(3000);
+
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
   module.exports = app;
 
-  console.log('Server running at http://localhost:3000/');
+  //console.log('Server running at http://localhost:3000/');
